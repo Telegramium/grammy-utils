e8faafa69e66054509e99c12a05d193a31809e92
BoatsDawn <140273525+BoatsDawn@users.noreply.github.com>
2025-12-02 21:52:12 +0300

    init

---
+++ README.md
@@ -0,0 +1,9 @@
+# @telegramium/grammy-utils
+
+Utility functions for grammY Telegram bots.
+
+## Installation
+
+```bash
+npm install @telegramium/grammy-utils
+```
---
+++ src/index.ts
@@ -0,0 +1,14 @@
+/**
+ * Escapes HTML characters in a string
+ * @param text The string to escape
+ * @returns The escaped string
+ */
+export const escapeHtml = (text: string): string => {
+    return text
+        .replace(/&/g, '&amp;')
+        .replace(/</g, '&lt;')
+        .replace(/>/g, '&gt;')
+        .replace(/"/g, '&quot;')
+        .replace(/'/g, '&#039;');
+};
+
